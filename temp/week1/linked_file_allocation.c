68c80cfa56fcdad3b970a69826f9f8106ffbbdc7
Developer Ashwin <developbyashwin@gmail.com>
2025-12-11 11:03:30 -0900

    Create linked_file_allocation.c

---
+++ temp/week1/linked_file_allocation.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <string.h>
+
+struct File {
+    char name[20];
+    int startBlock;
+    int numBlocks;
+    int blocks[100];
+};
+
+int main() {
+    int n, i, j;
+    printf("Enter number of files : ");
+    scanf("%d", &n);
+
+    struct File files[n];
+
+    for(i=0;i<n;i++){
+        printf("Enter file %d name : ", i+1);
+        scanf("%s", files[i].name);
+        printf("Enter starting block of file %d : ", i+1);
+        scanf("%d", &files[i].startBlock);
+        printf("Enter no of blocks in file %d : ", i+1);
+        scanf("%d", &files[i].numBlocks);
+        printf("Enter blocks for file %d : ", i+1);
+        for(j=0;j<files[i].numBlocks;j++)
+            scanf("%d",&files[i].blocks[j]);
+    }
+
+    printf("\nFile Name\tStart block\tNo. of blocks\tBlocks occupied\n");
+    for(i=0;i<n;i++){
+        printf("%-12s\t%-11d\t%-13d\t", files[i].name, files[i].startBlock, files[i].numBlocks);
+        for(j=0;j<files[i].numBlocks;j++){
+            printf("%d", files[i].blocks[j]);
+            if(j != files[i].numBlocks-1) printf(", ");
+        }
+        printf("\n");
+    }
+
+    char searchName[20];
+    printf("\nEnter the file name to be searched : ");
+    scanf("%s", searchName);
+
+    int found = 0;
+    for(i=0;i<n;i++){
+        if(strcmp(files[i].name, searchName) == 0){
+            printf("File '%s' found. Blocks occupied: ", searchName);
+            for(j=0;j<files[i].numBlocks;j++){
+                printf("%d", files[i].blocks[j]);
+                if(j != files[i].numBlocks-1) printf(", ");
+            }
+            printf("\n");
+            found = 1;
+            break;
+        }
+    }
+
+    if(!found)
+        printf("File '%s' not found.\n", searchName);
+
+    return 0;
+}
